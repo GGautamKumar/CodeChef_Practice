eca472adf6c0de6a3a3c2a512fa60d1a79c98c04
Gautam <0001gautamkumar123456@gmail.com>
2026-01-06 18:57:44 +0530

    Add SQL queries for product pricing, average salary, player details, and various data retrieval tasks

---
+++ Day2/Q10_Player_Details.sql
@@ -0,0 +1,29 @@
+-- Player Details
+-- Task
+-- Write a query to retrieve the details of the last five matches played, including the match ID, the names of the players who participated, the name of the winning player, match date, and the final score of the winner.
+
+-- There are two tables named Players and Matches.
+
+-- Tables
+
+-- Expected Output Columns:
+-- match_id
+-- player_1
+-- player_2
+-- winner
+-- match_date
+-- score
+-- Expected Output:
+-- ┌──────────┬──────────┬──────────┬────────┬────────────┬───────┐
+-- │ match_id │ player_1 │ player_2 │ winner │ match_date │ score │
+-- ├──────────┼──────────┼──────────┼────────┼────────────┼───────┤
+-- │ 106      │ Frank    │ Hank     │ Frank  │ 2024-01-29 │ 1450  │
+-- │ 101      │ Alice    │ Bob      │ Bob    │ 2024-01-25 │ 1500  │
+-- │ 110      │ David    │ Eve      │ David  │ 2024-01-24 │ 1600  │
+-- │ 108      │ Jack     │ Alice    │ Jack   │ 2024-01-19 │ 1400  │
+-- │ 103      │ Eve      │ Bob      │ Bob    │ 2024-01-17 │ 1500  │
+-- └──────────┴──────────┴──────────┴────────┴────────────┴───────┘
+
+
+
+select m.match_id as match_id,m.player_1 as player_1, m.player_2 as player_2, m.winner as winner ,m.match_date as match_date, p.score as score from Matches as m Join Players as p On p.player_name=m.winner order by m.match_date desc limit 5; 
